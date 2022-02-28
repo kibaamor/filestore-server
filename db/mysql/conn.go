d8a6932145637eb30a4e96567965d81983e240d0
kibaamor <5093911+kibaamor@users.noreply.github.com>
2022-02-28 23:52:38 +0800

    save data into mysql

---
+++ db/mysql/conn.go
@@ -0,0 +1,27 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func init() {
+	db, _ = sql.Open("mysql", "root:filestore@tcp(127.0.0.1:33061)/fileserver?charset=utf8mb4")
+
+	db.SetMaxOpenConns(1000)
+	err := db.Ping()
+	if err != nil {
+		fmt.Println("Failed to connect to mysql, err:" + err.Error())
+		os.Exit(1)
+	}
+}
+
+// DBConn:返回数据库连接对象
+func DBConn() *sql.DB {
+	return db
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/kibaamor/filestore-server
 go 1.17
 
 replace github.com/kibaamor/filestore-server => ./
+
+require github.com/go-sql-driver/mysql v1.6.0
