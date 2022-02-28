697d9561a863744a70fd9a9d05e4132e235db9b1
kibaamor <5093911+kibaamor@users.noreply.github.com>
2022-02-28 16:03:12 +0800

    fix mysql master slave.

--- docker/mysql/sql/master/create_sync_user.sql
+++ docker/mysql/sql/master/create_sync_user.sql
@@ -1,3 +1,3 @@
-create user 'repl'@'%' identified with 'mysql_native_password' by 'kibazen.cn';
+create user 'repl'@'%' identified by 'kibazen.cn';
 grant replication slave,replication client on *.* to 'repl'@'%';
 flush privileges;
--- scripts/mysql/sql/start_slave.sql
+++
@@ -1,2 +0,0 @@
-change master to master_host='mysql_master',master_user='repl',master_password='kibazen.cn';
-start slave;
