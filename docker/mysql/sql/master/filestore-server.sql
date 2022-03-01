14b9aa7949cd87eb78aa10299ffd9a9389cd53e4
kibaamor <5093911+kibaamor@users.noreply.github.com>
2022-03-01 23:43:52 +0800

    auth

--- docker/mysql/sql/master/filestore-server.sql
+++ docker/mysql/sql/master/filestore-server.sql
@@ -1,19 +1,40 @@
 CREATE DATABASE fileserver DEFAULT CHARACTER SET utf8mb4;
-
 USE fileserver;
-
 CREATE TABLE `tbl_file` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `file_sha1` char(40) NOT NULL DEFAULT '' COMMENT '文件hash',
-    `file_name` varchar(256) NOT NULL DEFAULT '' COMMENT '文件名',
-    `file_size` bigint(20) DEFAULT '0' COMMENT '文件大小',
-    `file_addr` varchar(1024) NOT NULL DEFAULT '' COMMENT '文件存储位置',
-    `create_at` datetime DEFAULT NOW() COMMENT '创建日期',
-    `update_at` datetime DEFAULT NOW() on update current_timestamp() COMMENT '更新日期',
-    `status` int(11) NOT NULL DEFAULT '0' COMMENT '状态（可用/禁用/已删除等状态）',
-    `ext1` int(11) DEFAULT '0' COMMENT '备用字段1',
-    `ext2` text COMMENT '备用字段2',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `idx_file_hash` (`file_sha1`),
-    KEY `idx_status` (`status`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `file_sha1` CHAR(40) NOT NULL DEFAULT '' COMMENT '文件hash',
+  `file_name` VARCHAR(256) NOT NULL DEFAULT '' COMMENT '文件名',
+  `file_size` BIGINT(20) DEFAULT '0' COMMENT '文件大小',
+  `file_addr` VARCHAR(1024) NOT NULL DEFAULT '' COMMENT '文件存储位置',
+  `create_at` DATETIME DEFAULT NOW() COMMENT '创建日期',
+  `update_at` DATETIME DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日期',
+  `status` INT(11) NOT NULL DEFAULT '0' COMMENT '状态（可用/禁用/已删除等状态）',
+  `ext1` INT(11) DEFAULT '0' COMMENT '备用字段1',
+  `ext2` TEXT COMMENT '备用字段2',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_file_hash` (`file_sha1`),
+  KEY `idx_status` (`status`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+CREATE TABLE `tbl_user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '用户名',
+  `user_pwd` VARCHAR(256) NOT NULL DEFAULT '' COMMENT '用户encoded密码',
+  `email` VARCHAR(64) DEFAULT '' COMMENT '邮箱',
+  `phone` VARCHAR(128) DEFAULT '' COMMENT '手机号',
+  `email_validated` TINYINT(1) DEFAULT 0 COMMENT '邮箱是否已验证',
+  `phone_validated` TINYINT(1) DEFAULT 0 COMMENT '手机号是否已验证',
+  `signup_at` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '注册日期',
+  `last_active` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后活跃时间戳',
+  `profile` TEXT COMMENT '用户属性',
+  `status` INT(11) NOT NULL DEFAULT '0' COMMENT '账户状态（启用/禁用/锁定/标记删除等）',
+  PRIMARY KEY(`id`),
+  UNIQUE KEY `idx_phone` (`phone`),
+  KEY `idx_status` (`status`)
+) ENGINE = InnoDB AUTO_INCREMENT = 5 DEFAULT CHARSET = utf8mb4;
+CREATE TABLE `tbl_user_token` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '用户名',
+  `user_token` CHAR(40) NOT NULL DEFAULT '' COMMENT '用户登录token',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_username` (`user_name`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
--- main.go
+++ main.go
@@ -8,12 +8,18 @@ import (
 )
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
 	http.HandleFunc("/file/upload", handler.UpdateHandler)
 	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
 	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
 	http.HandleFunc("/file/download", handler.DownloadHandler)
 	http.HandleFunc("/file/update", handler.FileUpdateMetaHandler)
 	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
+
+	http.HandleFunc("/user/signup", handler.SignupHandler)
+	http.HandleFunc("/user/signin", handler.SigninHandler)
+	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
